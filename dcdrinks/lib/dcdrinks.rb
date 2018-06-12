0f4a0919d4155fc41dd322c1e104330caa73e81a
Carrie Savinell <carrielane89@gmail.com>
2018-06-12 22:00:27 +0000

    Make changes

---
+++ dcdrinks/lib/dcdrinks.rb
@@ -0,0 +1,5 @@
+require "dcdrinks_gem/version"
+
+module DcdrinksGem
+  # Your code goes here...
+end
---
+++ dcdrinks/lib/dcdrinks_gem/venue.rb
@@ -0,0 +1,12 @@
+class DCDrinks::Venue 
+  
+  attr_accessor :name, :location, :url, :hh_time, :reviews
+  
+  def initialize 
+  end 
+  
+  def self.new
+  end   
+end   
+  
+  
